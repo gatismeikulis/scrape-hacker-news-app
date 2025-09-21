b6ff4e43f3428a7e1f5970b2201205faaad9f319
gmeikulis <gatis.meikulis@gmail.com>
2025-09-21 21:44:57 +0300

    Add 'tanstack-query' lib to fetch articles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,44 @@
+import { useQuery } from "@tanstack/react-query";
 import "./App.css";
+import { useState } from "react";
+
+async function fetchArticles(page: number) {
+  const response = await fetch(
+    `http://localhost:5000/api/articles?page=${page}`,
+  );
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  const jsonData: unknown = await response.json();
+  // consider using zod to validate response
+
+  return jsonData;
+}
 
 export function App() {
-  return <div className="demo-text">CLEAN VITE REACT APP</div>;
+  const [page, setPage] = useState(1);
+  console.log(page);
+
+  const { data, status } = useQuery({
+    queryKey: ["scrape-hacker-news", page],
+    queryFn: () => fetchArticles(page),
+  });
+
+  if (status === "pending") {
+    return <div>Loading...</div>;
+  }
+
+  if (status === "error") {
+    return <div>Error</div>;
+  }
+
+  console.log(data);
+
+  return (
+    <div className="demo-text">
+      CLEAN VITE REACT APP
+      <button onClick={() => setPage(page + 1)}>Next Page</button>
+      <button onClick={() => setPage(page - 1)}>Previous Page</button>
+    </div>
+  );
 }
